eb712f831224d6e450fd892a4e9dabc5836432c6
anusankanal <anusankanal@gmail.com>
2025-11-22 11:55:14 +0530

    Update marks.py

--- marks.py
+++ marks.py
@@ -1,23 +1,21 @@
-marks = []
-for i in range(1, 6):
-    mark = float(input(f"Enter marks for subject {i}: "))
-    marks.append(mark)
+marks1 = float(input("Enter marks of subject 1: "))
+marks2 = float(input("Enter marks of subject 2: "))
+marks3 = float(input("Enter marks of subject 3: "))
+marks4 = float(input("Enter marks of subject 4: "))
+marks5 = float(input("Enter marks of subject 5: "))
 
+avg = (marks1 + marks2 + marks3 + marks4 + marks5) / 5
 
-average = sum(marks) / 5
-
-
-if average >= 90:
+if avg >= 85:
     grade = "A"
-elif average >= 75:
+elif avg >= 70:
     grade = "B"
-elif average >= 60:
+elif avg >= 55:
     grade = "C"
-elif average >= 40:
+elif avg >= 40:
     grade = "D"
 else:
     grade = "Fail"
 
-
-print(f"\nAverage Marks: {average:.2f}")
-print(f"Grade: {grade}")
+print("Average Marks:", avg)
+print("Grade:", grade)
